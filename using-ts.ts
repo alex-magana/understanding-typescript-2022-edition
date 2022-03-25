7d0234bab3cd94c233f77dd2543767145d3253c7
alex-amagana <alex.magana@andela.com>
2022-03-25 15:59:55 +0300

    Add typescript specific examples

--- using-ts.ts
+++ using-ts.ts
@@ -1,25 +1,17 @@
 const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
+// The exclamation (!) instructs typescript that the element
+// will never yield null.
+// HTMLInputElement employs the use use a syntax called type casting
+// to instruct typescript what form the derived value will take.
+const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2");
 
 // <control>.value return a STRING.
 // For this reason adding input1.value
 // and input2.value results in a concatenated string instead
 // of the desired artithmetic addiotn operation
-// function add(num1, num2) {
-//   return num1 + num2;
-// }
-
-// The above described scenario canbe resolved by the fix
-// below, where we type check the arguments and handle them
-// accordingly.
-// This is where typescript comes in handy
 function add(num1, num2) {
-  if(typeof num1 === 'number' && typeof num2 === 'number') {
-    return num1 + num2;
-  } else {
-    return +num1 + +num2;
-  }
+  return num1 + num2;
 }
 
 button.addEventListener("click", function() {
