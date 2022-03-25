49cea65e2a6f4528f2328e47999c78fa10b400be
alex-amagana <alex.magana@andela.com>
2022-03-25 15:11:35 +0300

    Describe fix

--- js-only.js
+++ js-only.js
@@ -6,8 +6,20 @@ const input2 = document.getElementById("num2");
 // For this reason adding input1.value
 // and input2.value results in a concatenated string instead
 // of the desired artithmetic addiotn operation
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// The above described scenario canbe resolved by the fix
+// below, where we type check the arguments and handle them
+// accordingly.
+// This is where typescript comes in handy
 function add(num1, num2) {
-  return num1 + num2;
+  if(typeof num1 === 'number' && typeof num2 === 'number') {
+    return num1 + num2;
+  } else {
+    return +num1 + +num2;
+  }
 }
 
 button.addEventListener("click", function() {
